611a89ccf8b99ecc60cfaef6ff0192b9f7cbfd32
sonu-singh <sonusinghanpcs@gmail.com>
2024-07-25 21:07:37 +0530

    day_4

---
+++ day_4/loops.js
@@ -0,0 +1,50 @@
+// task 1
+for(let i=1; i<=10; i++){
+    //console.log(i);
+}
+
+// task 2
+for(let i =1;i<=10; i++){
+    //console.log(`5*${i}=${5*i}`);
+}
+
+// task 3
+let i = 10;
+let sum=0;
+while(i>=1){
+    sum+=i
+    i--
+}
+//console.log(sum);
+
+// task 4
+let j = 10;
+while(j>=1){
+    //console.log(j);
+    j--
+}
+
+// task 5
+let k = 1;
+do{
+    //console.log(k)
+    k+=1
+}while(k<=5);
+
+// task 6
+let fact = 1;
+let m = 5;
+do{
+    fact *= m
+    m--
+} while(m>=1);
+
+console.log(fact);
+
+// task 7
+str =""
+
+for(let n=1; n<=5; n++){
+    str+="*"
+    console.log(str)
+}
