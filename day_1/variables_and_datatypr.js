68b1d20cd8bede32e0952ee1038df97bcf427132
sonu-singh <sonusinghanpcs@gmail.com>
2024-07-25 19:24:18 +0530

    first commit

---
+++ day_1/variables_and_datatypr.js
@@ -0,0 +1,29 @@
+var num = 10;
+console.log(num);
+
+let str = "Hello World!";
+console.log(str);
+
+const boolean = true;
+console.log(boolean);
+
+
+const nums = 121;
+console.log(typeof nums);
+
+const string = "sonu";
+console.log(typeof string);
+
+const obj = { name: "John", age: 30 };
+console.log(typeof obj);
+
+const arr = [1, 2, 3];
+console.log(typeof arr);
+
+let check = 5;
+console.log(check);
+check = "hello";
+console.log(check);
+
+const err = 33
+//err = "new value"; // Error: Assignment to constant variable.
